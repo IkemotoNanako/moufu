1e09f7bd718c9de80bd485f8d93756c9a10fb605
IkemotoNanako <chibencaizi@gmail.com>
2024-07-19 19:22:26 +0900

    feat: ドメイン作成

---
+++ lib/domain/graph_data.dart
@@ -0,0 +1,13 @@
+class BodyWeightData {
+  final DateTime date;
+  final double weight;
+
+  BodyWeightData(this.date, this.weight);
+}
+
+class BodyFatPercentageData {
+  final DateTime date;
+  final double bodyFatPercentage;
+
+  BodyFatPercentageData(this.date, this.bodyFatPercentage);
+}
