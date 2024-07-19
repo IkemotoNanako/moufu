4f802bd88a5983b774ac84c6de6e1d9dea551fcc
IkemotoNanako <chibencaizi@gmail.com>
2024-07-19 19:32:39 +0900

    feat: めり込みボタン作成

--- lib/ui/page/graph_page.dart
+++ lib/ui/page/graph_page.dart
@@ -5,7 +5,28 @@ class GraphPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeScheme = Theme.of(context).colorScheme;
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: themeScheme.inversePrimary,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+        ),
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: themeScheme.primaryContainer,
+        surfaceTintColor: Colors.transparent,
+        notchMargin: 11,
+        shape: const AutomaticNotchedShape(
+          RoundedRectangleBorder(),
+          StadiumBorder(
+            side: BorderSide(),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Container(),
       ),
