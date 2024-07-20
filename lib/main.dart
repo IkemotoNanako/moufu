961620093e7525de0638249dd1c3b22363b95c99
IkemotoNanako <chibencaizi@gmail.com>
2024-07-21 00:53:12 +0900

    feat: id追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:moufu/ui/page/graph/graph_page.dart';
@@ -6,6 +7,7 @@ import 'package:moufu/ui/page/graph/graph_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
+  await dotenv.load(fileName: '.env');
 
   runApp(const ProviderScope(child: MyApp()));
 }
--- lib/ui/utils/get_addmod_id.dart
+++ lib/ui/utils/get_addmod_id.dart
@@ -1,19 +1,23 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String getAdBannerUnitId() {
+  final String androidId = dotenv.env['ANDROID_ADMOB_ID'] ?? '';
+  final String iosId = dotenv.env['IOS_ADMOB_ID'] ?? '';
+
   String bannerUnitId = "";
   if (Platform.isAndroid) {
     // Android のとき
     bannerUnitId = kDebugMode
         ? "ca-app-pub-3940256099942544/6300978111" // Androidのデモ用バナー広告ID
-        : "YOUR_ADMOB_ANDROID_UNIT_ID";
+        : androidId;
   } else if (Platform.isIOS) {
     // iOSのとき
     bannerUnitId = kDebugMode
         ? "ca-app-pub-3940256099942544/2934735716" // iOSのデモ用バナー広告ID
-        : "YOUR_ADMOB_IOS_UNIT_ID";
+        : iosId;
   }
   return bannerUnitId;
 }
