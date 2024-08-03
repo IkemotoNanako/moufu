c5997487903a9d42e1cc387b6c4a7d687eb60e75
IkemotoNanako <chibencaizi@gmail.com>
2024-08-03 18:45:31 +0900

    feat: 広告追加

--- lib/ui/utils/get_addmod_id.dart
+++ lib/ui/utils/get_addmod_id.dart
@@ -21,3 +21,22 @@ String getAdBannerUnitId() {
   }
   return bannerUnitId;
 }
+
+String getAdBannerUnitIdSecond() {
+  final String androidId = dotenv.env['ANDROID_ADMOB_ID'] ?? '';
+  final String iosId = dotenv.env['IOS_ADMOB_ID_SECOND'] ?? '';
+
+  String interstitialUnitId = "";
+  if (Platform.isAndroid) {
+    // Android のとき
+    interstitialUnitId = kDebugMode
+        ? "ca-app-pub-3940256099942544/6300978111" // Androidのデモ用インタースティシャル広告ID
+        : androidId;
+  } else if (Platform.isIOS) {
+    // iOSのとき
+    interstitialUnitId = kDebugMode
+        ? "ca-app-pub-3940256099942544/2934735716" // iOSのデモ用バナー広告ID
+        : iosId;
+  }
+  return interstitialUnitId;
+}
