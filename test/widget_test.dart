830edd0ba62ac5feb4d0ae0d1a9e659fcd492d53
IkemotoNanako <chibencaizi@gmail.com>
2024-08-04 11:17:18 +0900

    feat: テストのエラー解消

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:moufu/domain/theme_type.dart';
 
 import 'package:moufu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(
+      themeType: ThemeType.orange,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
